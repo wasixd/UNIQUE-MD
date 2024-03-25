6e79bc053e65056e55eac28af6549989000ac73e
wasixd <164501586+wasixd@users.noreply.github.com>
2024-03-25 16:00:36 +0500

    UNIQUE-MD

---
+++ plugins/main-creator.js
@@ -0,0 +1,22 @@
+var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let pp = princeImg.getRandom()	
+const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+
+
+*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+
+*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*---------------------*
+
+
+*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
+*${developer}*`
+
+await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
+
+export default handler
